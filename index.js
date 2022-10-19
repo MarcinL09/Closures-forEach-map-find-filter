fa509588e2860aba0a0275532396dac2d74f8364
Marcin Laszczynski <mar.zczsal@gmail.com>
2022-10-19 21:56:07 +0200

    added getSquaredNumber function

--- index.js
+++ index.js
@@ -12,3 +12,23 @@ function getSmallestNumber(arrayOfNumbers) {
 
 console.log(getSmallestNumber([2, -5, 10, 1, 4])); // -5
 console.log(getSmallestNumber([200, 25, 4, 123, 87])); // 4
+
+// write the getSquaredNumbers function using map
+const arrayOfNumbers = [1,2,3,4,5]
+// function getSquaredNumbers(arrayOfNumbers) {
+//     return arrayOfNumbers ** 2;
+// }
+    const arrayOfSquareNumbers = arrayOfNumbers.map(function getSquaredNumber(arrayOfNumbers) {
+        return arrayOfNumbers ** 2;
+});
+    // const numberToBeSquared = arrayOfNumbers
+//     for (let i = 0; i < arrayOfNumbers.length; ++i) {
+//         const number = arrayOfNumbers[i]
+//         arrayOfSquareNumbers.push(number ** 2)
+//     }
+//     return arrayOfSquareNumbers;
+// }
+
+console.log(arrayOfSquareNumbers);
+console.log(getSquaredNumbers([1, 2, 3, 4, 5])); // [1, 4, 9, 16, 25]
+console.log(getSquaredNumbers([6, 7, 8, 9, 10])); // [36, 49, 64, 81, 100]
