d02f0c5df188899e5f9b599126367490faf06545
Marcin Laszczynski <mar.zczsal@gmail.com>
2022-11-02 20:33:10 +0100

    added 'executeAfterFiveSecond' function

--- index.js
+++ index.js
@@ -227,5 +227,16 @@ function isFruit(fruit) {
 
 find(fruits, isFruit);
 
+// Additional exercises
 
+// create the executeAfterFiveSeconds function that calls the provided function after five seconds
 
+function executeAfterFiveSeconds(functionRef) {
+    setTimeout(functionRef, 5000);
+}
+
+function sayHello() {
+    console.log('Hello!');
+}
+
+executeAfterFiveSeconds(sayHello);
